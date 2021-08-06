58822720cb7c21ed597cb5cf806e48c95054610a
rahadians <matrix.black82@gmail.com>
2021-08-06 12:06:18 +0700

    finnished day3 route dan vegetable tidak dihapus

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:catalogdelapan/pages/login_page.dart';
 import 'package:flutter/material.dart';
-import 'package:catalogdelapan/home_page.dart';
+import 'package:catalogdelapan/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +11,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bringVegetables(thaila: false);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (cotext) => new LoginPage(),
+        "/home": (cotext) => HomePage(),
+        "/Login": (cotext) => LoginPage(),
+      },
     );
-    // test day 3
+  }
+
+  bringVegetables({
+    @required bool thaila = false,
+    int rupiah = 100,
+  }) {
+    //Take Cycle
+
+    //Go to sec 16
   }
 }
