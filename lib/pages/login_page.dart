5ec812d9ddb3e7c4cd9432ddc17d8975e1886388
rahadians <matrix.black82@gmail.com>
2021-08-09 17:54:32 +0700

    days 6 finnish

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,47 +10,52 @@ class LoginPage extends StatelessWidget {
     return Material(
       color: Colors.white,
       child: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image(
-            image: AssetImage('assets/images/mobile_login_2.png'),
-            fit: BoxFit.fill,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 2,
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16, 32, 16, 32),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Input Username", labelText: "Username"),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      hintText: "Password", labelText: "Password"),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                ElevatedButton(onPressed: () {
-                  print("Hi Rahadian");
-                }, child: Text("Login"),
-                style: TextButton.styleFrom())
-              ],
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              image: AssetImage(
+                'assets/images/mobile_login_2.png',
+              ),
+              fit: BoxFit.fill,
+              height: 400,
             ),
-          ),
-        ],
+            SizedBox(height: 20),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 32, 16, 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Input Username", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("Hi Rahadian");
+                      },
+                      child: Text("Login"),
+                      style: TextButton.styleFrom())
+                ],
+              ),
+            ),
+          ],
+        ),
       )),
     );
   }
