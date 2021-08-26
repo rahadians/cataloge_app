47761d9093e4fba0c8f74871e7cd132bdf509cc3
rahadians <matrix.black82@gmail.com>
2021-08-26 11:48:47 +0700

    Pelajaran hari ke 13

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,39 @@
+import 'dart:convert';
+
 import 'package:catalogdelapan/models/catalog.dart';
 import 'package:catalogdelapan/widget/item_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:catalogdelapan/widget/drawer.dart';
+import 'package:flutter/services.dart';
 
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   // const HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "rahadian";
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    LoadData();
+  }
+
+  LoadData() async {
+    final catalogJson = await rootBundle.loadString("assets/files/catale.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData=decodeData("products");
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummyList=List.generate(50, (index) => CatalogModel.items[0]);
+    final dummyList = List.generate(50, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Center(
@@ -23,12 +44,10 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context,index){
-            return ItemWidget(
-                item: dummyList[index]);
-          }
-        ),
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: dummyList[index]);
+            }),
       ),
       drawer: MyDrawer(),
     );
